bcc46a3ba64b4a250418b4d8881fb359ee788a57
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2024-04-18 17:23:27 +0100

    Project Intialised added tailwind css

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import IntroPage from "./components/IntroPage";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <IntroPage></IntroPage>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/IntroPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>
+        {" "}
+        Welcome to the <span> MERN</span> Stack
+      </h1>
+      <p> This is a simple MERN stack application</p>
+    </div>
+  );
+}
