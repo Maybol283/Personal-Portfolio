fadb48c4b72d622ac60cdd2513436080bdff3a85
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2024-04-23 21:34:34 +0100

    added images to marquee

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -4,13 +4,26 @@ import { useSpring, animated } from "@react-spring/web";
 function Projects() {
   const props = useSpring({
     from: { transform: "translateX(0%)" },
-    to: { transform: "translateX(-100%)" },
+    to: { transform: "translateX(-300%)" },
     loop: true,
-    config: { duration: 20000 },
+    config: { duration: 50000 },
   });
 
-  const items = [1, 2, 3, 4]; // List of items
-  const duplicatedItems = [...items, ...items]; // Triple the items for a smoother loop
+  const items = [
+    {
+      src: "src/assets/Meo-Romano.PNG",
+      alt: "Meo Romano",
+    },
+    {
+      src: "src/assets/Markdown-Previewer.png",
+      alt: "Markdown Maker",
+    },
+    {
+      src: "src/assets/Quote-Generator.png",
+      alt: "Quote Generator",
+    },
+  ]; // List of items
+  const duplicatedItems = [...items, ...items, ...items]; // Triple the items for a smoother loop
 
   return (
     <div className="size-full text-center overflow-hidden">
@@ -22,9 +35,13 @@ function Projects() {
         {duplicatedItems.map((item, index) => (
           <div
             key={index}
-            className="flex-none w-96 bg-white text-palette-2 border-2"
+            className="flex-none w-3/6  bg-white text-palette-2 border-2 "
           >
-            <p>{item}</p>
+            <img
+              className="object-cover size-full"
+              src={item.src}
+              alt={item.alt}
+            />
           </div>
         ))}
       </animated.div>
